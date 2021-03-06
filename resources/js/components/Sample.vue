d3103796eee5b0cd8eb2f9eb9e05a3533b0016dd
Ray Castel <jrlc1107@gmail.com>
2021-03-06 19:21:14 +0800

    add vue & axios non-cdn

---
+++ resources/js/components/Sample.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        SAMPLEEEEEEE
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
