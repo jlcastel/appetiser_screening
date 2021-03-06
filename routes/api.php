2af66d60bd463f14868aa138e922047ac994dcc0
Ray Castel <jrlc1107@gmail.com>
2021-03-06 16:42:26 +0800

    add test api endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ use App\Http\Controllers\EventController;
 
 Route::post('event/add', [EventController::class, 'store']);
 Route::get('events/', [EventController::class, 'getAllEvents']);
+Route::get('events/events/', [EventController::class, 'getAllEvents']);
